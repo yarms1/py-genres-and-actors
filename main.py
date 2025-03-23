00df3b84ecafe1eed4a6ae8a6c573362ddbbc4e1
Olexand <yarmus.olexandr@gmail.com>
2025-03-24 01:18:55 +0200

    task solution 5.0

--- main.py
+++ main.py
@@ -5,29 +5,30 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Drama")
+    genres = ["Western", "Action", "Dramma"]
+    for genre in genres:
+        Genre.objects.create(name=genre)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first, last in actors:
+        Actor.objects.create(
+            first_name=first,
+            last_name=last
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(
-        last_name="Clooney")
-
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-        first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves"
+                         ).update(first_name="Keanu", last_name="Reeves")
 
     Genre.objects.filter(name="Action").delete()
-
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    actors_with_smith = Actor.objects.filter(last_name="Smith").order_by(
-        "first_name")
-    return actors_with_smith
+    return Actor.objects.filter(last_name="Smith").all().order_by("first_name")
